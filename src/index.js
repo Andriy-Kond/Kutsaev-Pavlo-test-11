59165daa9152e33921a9bbfb23f1306b55c55c10
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-02-06 12:19:22 +0200

    7

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ImgApi from './axio.js';
+import ImgApi from './Axio.js';
 import LoadMoreBtn from './components/LoadMoreBtn.js';
 import Notiflix from 'notiflix';
 import axios from 'axios';
